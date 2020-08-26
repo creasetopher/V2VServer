34b54c73fb37deac62b6d0d2373a14f014771960
Chris Sims <chrissims@Chriss-MacBook-Pro.local>
2020-08-26 03:32:56 -0400

    before deploy

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, send_file, after_this_request
+from services.downloader_service import DownloadService
+from os import remove
+app = Flask(__name__)
+
+service = DownloadService()
+VALID_SYMBOLS = "!@#$%^&*()_-+={}[]"
+DIR_DELIMITER = "/"
+
+@app.route('/api/utils/v2v/validate')
+def validate():
+    url = request.args.get('url')
+    service.fetch(url)
+    return service.get_info()
+
+
+@app.route('/api/utils/v2v/download')
+def download():
+    # if user hits dl again, ensure it sends vid url in request to get stream again
+    # if not service.media:
+
+    download_path = service.get_audio()
+    filename = service.media.title
+    sanitized_filename = ""
+    for char in filename:
+        if not char.isalnum() and char not in VALID_SYMBOLS:
+            if char == DIR_DELIMITER:
+                sanitized_filename += "-"
+        else:
+            sanitized_filename += char
+
+    # filename = filename.replace('/', '-')
+
+    @after_this_request
+    def cleanup(response):
+        print("Deleting audio file")
+        remove(download_path)
+        return response
+
+    return send_file(download_path,
+                     attachment_filename = sanitized_filename + '.mp3')
